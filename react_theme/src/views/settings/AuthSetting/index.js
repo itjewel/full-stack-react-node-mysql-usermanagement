02852feac27b5fbb0b94ba09f73b2df77fd76eab
Jewel Farazi <md.jewelfarazi@gmail.com>
2022-02-17 19:34:23 +0600

    Auth setting added

--- react_theme/src/views/settings/AuthSetting/index.js
+++ react_theme/src/views/settings/AuthSetting/index.js
@@ -23,8 +23,8 @@ function AuthSetting() {
       <CCol xs={12}>
         <CCardBody>
           <CForm className="row g-3">
-            <CCol md={3}>
-              <CFormLabel htmlFor="inputEmail8">Stripe Payment</CFormLabel>
+            <CCol md={4}>
+              <CFormLabel htmlFor="inputEmail8">Stripe Payment</CFormLabel> <br />
               <CButtonGroup role="group" aria-label="Basic checkbox toggle button group">
                 <CFormCheck
                   type="radio"
@@ -33,6 +33,7 @@ function AuthSetting() {
                   id="stripe_payment_status1"
                   autoComplete="off"
                   label="YES"
+                  defaultChecked
                 />
                 <CFormCheck
                   type="radio"
@@ -44,10 +45,8 @@ function AuthSetting() {
                 />
               </CButtonGroup>
             </CCol>
-          </CForm>
-          <CForm>
-            <CCol md={3}>
-              <CFormLabel htmlFor="inputEmail8">SSLCOMMERZ Payment</CFormLabel>
+            <CCol md={4}>
+              <CFormLabel htmlFor="inputEmail8">SSLCOMMERZ Payment</CFormLabel> <br />
               <CButtonGroup role="group" aria-label="Basic checkbox toggle button group">
                 <CFormCheck
                   type="radio"
@@ -62,6 +61,119 @@ function AuthSetting() {
                   button={{ color: 'danger', variant: 'outline' }}
                   name="sslcommerz_payment_status"
                   id="sslcommerz_payment_status2"
+                  autoComplete="off"
+                  label="OFF"
+                  defaultChecked
+                />
+              </CButtonGroup>
+            </CCol>
+            <CCol md={4}>
+              <CFormLabel htmlFor="inputEmail8">Paypal Payment</CFormLabel> <br />
+              <CButtonGroup role="group" aria-label="Basic checkbox toggle button group">
+                <CFormCheck
+                  type="radio"
+                  button={{ color: 'primary', variant: 'outline' }}
+                  name="paypal_payment_status"
+                  id="paypal_payment_status1"
+                  autoComplete="off"
+                  label="YES"
+                />
+                <CFormCheck
+                  type="radio"
+                  button={{ color: 'danger', variant: 'outline' }}
+                  name="paypal_payment_status"
+                  id="paypal_payment_status2"
+                  autoComplete="off"
+                  label="OFF"
+                  defaultChecked
+                />
+              </CButtonGroup>
+            </CCol>
+            <CCol md={4}>
+              <CFormLabel htmlFor="inputEmail8">Allow "Remember Me"?</CFormLabel> <br />
+              <CButtonGroup role="group" aria-label="Basic checkbox toggle button group">
+                <CFormCheck
+                  type="radio"
+                  button={{ color: 'primary', variant: 'outline' }}
+                  name="remember_me"
+                  id="remember_me1"
+                  autoComplete="off"
+                  label="YES"
+                />
+                <CFormCheck
+                  type="radio"
+                  button={{ color: 'danger', variant: 'outline' }}
+                  name="remember_me"
+                  id="remember_me2"
+                  autoComplete="off"
+                  label="OFF"
+                  defaultChecked
+                />
+              </CButtonGroup>
+            </CCol>
+            <CCol md={4}>
+              <CFormLabel htmlFor="inputEmail8">Forgot Password</CFormLabel> <br />
+              <CButtonGroup role="group" aria-label="Basic checkbox toggle button group">
+                <CFormCheck
+                  type="radio"
+                  button={{ color: 'primary', variant: 'outline' }}
+                  name="forget_password"
+                  id="forget_password1"
+                  autoComplete="off"
+                  label="YES"
+                />
+                <CFormCheck
+                  type="radio"
+                  button={{ color: 'danger', variant: 'outline' }}
+                  name="forget_password"
+                  id="forget_password2"
+                  autoComplete="off"
+                  label="OFF"
+                  defaultChecked
+                />
+              </CButtonGroup>
+            </CCol>
+            <CCol md={4}>
+              <CFormLabel htmlFor="inputEmail8">
+                {`Notify Administrators when user signs up?`}
+              </CFormLabel>
+              <CButtonGroup role="group" aria-label="Basic checkbox toggle button group">
+                <CFormCheck
+                  type="radio"
+                  button={{ color: 'primary', variant: 'outline' }}
+                  name="notify_signup"
+                  id="notify_signup1"
+                  autoComplete="off"
+                  label="YES"
+                />
+                <CFormCheck
+                  type="radio"
+                  button={{ color: 'danger', variant: 'outline' }}
+                  name="notify_signup"
+                  id="notify_signup2"
+                  autoComplete="off"
+                  label="OFF"
+                  defaultChecked
+                />
+              </CButtonGroup>
+            </CCol>
+            <CCol md={4}>
+              <CFormLabel htmlFor="inputEmail8">Google reCAPTCHA</CFormLabel> <br />
+              <CButtonGroup role="group" aria-label="Basic checkbox toggle button group">
+                <CFormCheck
+                  type="radio"
+                  button={{ color: 'primary', variant: 'outline' }}
+                  name="re_capcha"
+                  id="re_capcha1"
+                  autoComplete="off"
+                  label="YES"
+                  defaultChecked
+                />
+                <CFormCheck
+                  type="radio"
+                  button={{ color: 'danger', variant: 'outline' }}
+                  name="re_capcha"
+                  id="re_capcha2"
                   autoComplete="off"
                   label="OFF"
                 />
--- react_theme/src/views/settings/ProfileSetting/index.js
+++ react_theme/src/views/settings/ProfileSetting/index.js
@@ -1,7 +1,157 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CIcon from '@coreui/icons-react'
+import * as icon from '@coreui/icons'
+import {
+  CNav,
+  CNavItem,
+  CNavLink,
+  CTabContent,
+  CTabPane,
+  CRow,
+  CCol,
+  CCard,
+  CCardBody,
+  CForm,
+  CFormLabel,
+  CFormInput,
+  CFormSelect,
+  CFormTextarea,
+  CButton,
+  CTable,
+  CTableHead,
+  CTableRow,
+  CTableHeaderCell,
+  CTableBody,
+  CTableDataCell,
+  CTooltip,
+  CImage,
+} from '@coreui/react'
+
+import profileImage from './../../../assets/images/avatars/001-profile.png'
 
 function ProfileSetting() {
-  return <div>Profile setting</div>
+  const [activeKey, setActiveKey] = useState(1)
+  return (
+    <>
+      <CNav variant="tabs" role="tablist">
+        <CNavItem>
+          <CNavLink href="#" active={activeKey === 1} onClick={() => setActiveKey(1)}>
+            <CIcon icon={icon.cilViewModule} /> Personal Info
+          </CNavLink>
+        </CNavItem>
+        <CNavItem>
+          <CNavLink href="#" active={activeKey === 2} onClick={() => setActiveKey(2)}>
+            <CIcon icon={icon.cilPlus} /> Auth Info
+          </CNavLink>
+        </CNavItem>
+        <CNavItem>
+          <CNavLink href="#" active={activeKey === 3} onClick={() => setActiveKey(3)}>
+            <CIcon icon={icon.cilPlus} /> Social Network
+          </CNavLink>
+        </CNavItem>
+        <CNavItem>
+          <CNavLink href="#" active={activeKey === 4} onClick={() => setActiveKey(4)}>
+            <CIcon icon={icon.cilPlus} /> Profile Picture
+          </CNavLink>
+        </CNavItem>
+      </CNav>
+
+      <CTabContent>
+        <CTabPane role="tabpanel" aria-labelledby="home-tab" visible={activeKey === 1}>
+          <CRow>
+            <CCol xs={12}>
+              <CCard className="mb-4">
+                <CCardBody>
+                  <CForm className="row g-3">
+                    <CCol md={8}>
+                      <CFormLabel htmlFor="inputEmail8">Role Name</CFormLabel>
+                      <CFormInput type="text" id="inputEmail4" />
+                    </CCol>
+                    <CCol md={8}>
+                      <CFormLabel htmlFor="inputEmail4">Display Name</CFormLabel>
+                      <CFormInput type="text" id="inputEmail4" />
+                    </CCol>
+                    <CCol md={8}>
+                      <CFormLabel htmlFor="inputAddress">Description</CFormLabel>
+                      <CFormTextarea id="exampleFormControlTextarea1" rows="3"></CFormTextarea>
+                    </CCol>
+                    <CCol md={12}>
+                      <CButton type="submit">Create Role</CButton>
+                    </CCol>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCol>
+          </CRow>
+        </CTabPane>
+        <CTabPane role="tabpanel" aria-labelledby="profile-tab" visible={activeKey === 2}>
+          <CRow>
+            <CCol xs={12}>
+              <CCard className="mb-4">
+                <CCardBody>
+                  <CForm className="row g-3">
+                    <CCol md={8}>
+                      <CFormLabel htmlFor="inputEmail8">Role Name</CFormLabel>
+                      <CFormInput type="text" id="inputEmail4" />
+                    </CCol>
+                    <CCol md={8}>
+                      <CFormLabel htmlFor="inputEmail4">Display Name</CFormLabel>
+                      <CFormInput type="text" id="inputEmail4" />
+                    </CCol>
+                    <CCol md={8}>
+                      <CFormLabel htmlFor="inputAddress">Description</CFormLabel>
+                      <CFormTextarea id="exampleFormControlTextarea1" rows="3"></CFormTextarea>
+                    </CCol>
+                    <CCol md={12}>
+                      <CButton type="submit">Create Role</CButton>
+                    </CCol>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCol>
+          </CRow>
+        </CTabPane>
+        <CTabPane role="tabpanel" aria-labelledby="profile-tab" visible={activeKey === 3}>
+          <CRow>
+            <CCol xs={12}>
+              <CCard className="mb-4">
+                <CCardBody>
+                  <CForm className="row g-3">
+                    <CCol md={8}>
+                      <CFormLabel htmlFor="inputEmail8">Role Name</CFormLabel>
+                      <CFormInput type="text" id="inputEmail4" />
+                    </CCol>
+                    <CCol md={8}>
+                      <CFormLabel htmlFor="inputEmail4">Display Name</CFormLabel>
+                      <CFormInput type="text" id="inputEmail4" />
+                    </CCol>
+                    <CCol md={8}>
+                      <CFormLabel htmlFor="inputAddress">Description</CFormLabel>
+                      <CFormTextarea id="exampleFormControlTextarea1" rows="3"></CFormTextarea>
+                    </CCol>
+                    <CCol md={12}>
+                      <CButton type="submit">Create Role</CButton>
+                    </CCol>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCol>
+          </CRow>
+        </CTabPane>
+      </CTabContent>
+      <CTabPane role="tabpanel" aria-labelledby="profile-tab" visible={activeKey === 4}>
+        <CRow>
+          <CCol xs={12}>
+            <CCard className="mb-4">
+              <CCardBody>
+                <CImage rounded thumbnail src={profileImage} width={200} height={200} />
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
+      </CTabPane>
+    </>
+  )
 }
 
 export default ProfileSetting
