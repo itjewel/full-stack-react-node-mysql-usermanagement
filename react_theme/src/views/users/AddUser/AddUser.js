36ed3a73c8d0663eb7870b9c3b10488d5e8d1f6a
jewel <md.jewelmia52@gmail.com>
2022-02-28 00:03:36 +0600

    validation added

--- react_theme/src/views/users/AddUser/AddUser.js
+++ react_theme/src/views/users/AddUser/AddUser.js
@@ -20,20 +20,33 @@ const AddUser = () => {
   const initialValues = {
     first_name: '',
     last_name: '',
-    phone_number: '',
+    mobile_number: '',
     email_address: '',
     username: '',
     password: '',
+    passwordConfirmation: '',
     date_of_birth: '',
+    address: '',
   }
   const validationSchema = Yup.object().shape({
     first_name: Yup.string().required('Please Enter First Name'),
     last_name: Yup.string().required('Please Enter Last Name'),
-    phone_number: Yup.string().min(3).max(15).required('Please Enter Phone Number'),
-    email_address: Yup.string().min(3).max(15).required('Please Enter email address'),
+    mobile_number: Yup.string().min(3).max(15).required('Please Enter Phone Number'),
+    email_address: Yup.string()
+      .email('Must be a valid email')
+      .max(255)
+      .required('Email is required'),
     username: Yup.string().min(3).max(15).required('Please Enter Username'),
-    password: Yup.string().min(3).max(15).required('Please Enter password'),
+    password: Yup.string().min(8).required('Password Required'),
+    passwordConfirmation: Yup.string().test(
+      'passwords-match',
+      'Passwords must match',
+      function (value) {
+        return this.parent.password === value
+      },
+    ),
     date_of_birth: Yup.string().min(3).max(15).required('Please Enter date of birth'),
+    address: Yup.string().required('Please Enter Address'),
   })
   const onSubmit = (data) => {
     axios.post('http://localhost:3001/posts', data).then((response) => {
@@ -53,14 +66,18 @@ const AddUser = () => {
             <CCardBody>
               <Form className="row g-3">
                 <CCol md={4}>
-                  <CFormLabel htmlFor="inputEmail4">First Name </CFormLabel>
+                  <CFormLabel htmlFor="inputEmail4">
+                    First Name <CIcon className="text-danger" icon={icon.cilAsterisk} />
+                  </CFormLabel>
                   <Field name="first_name" type="text" id="inputEmail4" className="form-control" />
-                  <ErrorMessage name="first_name" component="span" />
+                  <ErrorMessage name="first_name" className="text-danger" component="span" />
                 </CCol>
                 <CCol md={4}>
-                  <CFormLabel htmlFor="inputEmail4">Last Name</CFormLabel>
+                  <CFormLabel htmlFor="inputEmail4">
+                    <CIcon className="text-danger" icon={icon.cilAsterisk} /> Last Name
+                  </CFormLabel>
                   <Field name="last_name" type="text" id="inputEmail4" className="form-control" />
-                  <ErrorMessage name="last_name" component="span" />
+                  <ErrorMessage name="last_name" className="text-danger" component="span" />
                 </CCol>
                 <CCol md={4}>
                   <CFormLabel htmlFor="inputEmail4">Select Role</CFormLabel>
@@ -81,47 +98,82 @@ const AddUser = () => {
                   </CFormSelect>
                 </CCol>
                 <CCol md={4}>
-                  <CFormLabel htmlFor="inputZip">Phone</CFormLabel>
-                  <Field name="phone_number" id="inputZip" className="form-control" />
-                  <ErrorMessage name="phone_number" component="span" />
+                  <CFormLabel htmlFor="inputZip">
+                    Mobile <CIcon className="text-danger" icon={icon.cilAsterisk} />
+                  </CFormLabel>
+                  <Field name="mobile_number" id="inputZip" className="form-control" />
+                  <ErrorMessage className="text-danger" name="mobile_number" component="span" />
                 </CCol>
                 <CCol md={4}>
-                  <CFormLabel htmlFor="inputEmail4">E-Mail Address</CFormLabel>
+                  <CFormLabel htmlFor="inputEmail4">
+                    E-Mail Address <CIcon className="text-danger" icon={icon.cilAsterisk} />
+                  </CFormLabel>
                   <Field
                     type="email"
                     name="email_address"
                     id="inputEmail4"
                     className="form-control"
                   />
-                  <ErrorMessage name="email_address" component="span" />
+                  <ErrorMessage className="text-danger" name="email_address" component="span" />
                 </CCol>
                 <CCol md={4}>
-                  <CFormLabel htmlFor="inputEmail4">Username</CFormLabel>
+                  <CFormLabel htmlFor="inputEmail4">
+                    Username <CIcon className="text-danger" icon={icon.cilAsterisk} />
+                  </CFormLabel>
                   <Field type="text" name="username" id="inputEmail4" className="form-control" />
-                  <ErrorMessage name="username" component="span" />
+                  <ErrorMessage className="text-danger" name="username" component="span" />
                 </CCol>
                 <CCol md={4}>
-                  <CFormLabel htmlFor="inputEmail4">Password</CFormLabel>
-                  <Field type="text" name="password" id="inputEmail4" className="form-control" />
-                  <ErrorMessage name="password" component="span" />
+                  <CFormLabel htmlFor="inputEmail4">
+                    Password <CIcon className="text-danger" icon={icon.cilAsterisk} />
+                  </CFormLabel>
+                  <Field
+                    type="password"
+                    name="password"
+                    id="inputEmail4"
+                    className="form-control"
+                  />
+                  <ErrorMessage className="text-danger" name="password" component="span" />
                 </CCol>
                 <CCol md={4}>
-                  <CFormLabel htmlFor="inputEmail4">Confirm Password</CFormLabel>
-                  <CFormInput type="text" id="inputEmail4" />
+                  <CFormLabel htmlFor="inputEmail4">
+                    Confirm Password <CIcon className="text-danger" icon={icon.cilAsterisk} />
+                  </CFormLabel>
+                  <Field
+                    type="password"
+                    name="passwordConfirmation"
+                    id="inputEmail4"
+                    className="form-control"
+                  />
+                  <ErrorMessage
+                    name="passwordConfirmation"
+                    className="text-danger"
+                    component="span"
+                  />
                 </CCol>
                 <CCol md={4}>
-                  <CFormLabel htmlFor="inputEmail4">Date Of Birth</CFormLabel>
+                  <CFormLabel htmlFor="inputEmail4">
+                    Date Of Birth <CIcon className="text-danger" icon={icon.cilAsterisk} />
+                  </CFormLabel>
                   <Field
                     type="date"
                     name="date_of_birth"
                     id="inputPassword4"
                     className="form-control"
                   />
-                  <ErrorMessage name="date_of_birth" component="span" />
+                  <ErrorMessage name="date_of_birth" className="text-danger" component="span" />
                 </CCol>
                 <CCol md={8}>
-                  <CFormLabel htmlFor="inputAddress">Address</CFormLabel>
-                  <CFormTextarea id="exampleFormControlTextarea1" rows="3"></CFormTextarea>
+                  <CFormLabel htmlFor="inputAddress">
+                    Address <CIcon className="text-danger" icon={icon.cilAsterisk} />
+                  </CFormLabel>
+                  <Field
+                    name="address"
+                    as="textarea"
+                    className="form-control"
+                    placeholder="Address"
+                  />
+                  <ErrorMessage name="address" className="text-danger" component="span" />
                 </CCol>
                 <CCol md={12}>
                   <CButton type="submit">
